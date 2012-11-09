9acf61a0ab48f1408013cbd17ab01757d47d171e
David Klassen <f0rk.tt@gmail.com>
2012-11-09 18:06:30 +0700

    Added view locals and active links

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
+// set view locals
+app.use(function (req, res, next) {
+  app.locals.route = req.url
+  next()
+})
+
 // mount routes
 app.get('/', function (req, res) { res.redirect('home') })
 app.get('/home', pages.home)
