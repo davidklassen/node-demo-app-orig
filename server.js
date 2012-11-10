e728bf03ccadc3b9115f31ad3f2dd2dcd5d38a14
David Klassen <f0rk.tt@gmail.com>
2012-11-10 12:48:29 +0700

    Added Procfile and engines

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require(__dirname + '/app')
+  , port = process.env.PORT || 3000
 
-app.listen(3000, function () {
-  console.log('Listening on port ', 3000)
+app.listen(port, function () {
+  console.log('Listening on port ', port)
 })
 
